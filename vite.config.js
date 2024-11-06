29451b3c0adc1503b234de299cfc9252dcdd4e66
MuhammadUsman <usmanmgrfilza@gmail.com>
2024-11-07 01:00:10 +0500

    new

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-
-// https://vitejs.dev/config/
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/', // Ensure base path is set for Vercel
-  build: {
-    rollupOptions: {
-      input: './src/main.jsx', // Corrected entry point path
-    },
-  },
-});
-
+})
