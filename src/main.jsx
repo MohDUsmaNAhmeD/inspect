05ed315704b73b77524be303cd3a70c8c47fd4a0
MohdUsmanahmed <muhammadusmanahmed211@gmail.com>
2024-11-09 22:39:00 +0500

    lattest

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom/client'; // React 18 specific import
 import App from './App';
-import './index.css'; // Import your CSS file
+import './index.css';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root')); // Correct way to create the root element
 
 root.render(
-        <App />
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
